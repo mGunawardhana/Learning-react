a37e16b30f2a4586f91eb4713ba67bd9d8be8d9a
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-03-11 14:13:20 +0530

    sample text

--- src/App.tsx
+++ src/App.tsx
@@ -15,22 +15,7 @@ export default class App extends Component {
 
 render(): React.ReactNode {
  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App"><h1>Hello I'm mGunawardhana</h1></div>
   );
 }}
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    // mee onema file ekaka me hataren ekak nam file extension eka ewa tailwind walata adaalai
+    //eka ta me css class tika apply karanna
+    ".src/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {
     extend: {},
   },
